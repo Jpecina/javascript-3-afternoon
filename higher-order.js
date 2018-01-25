4e537618ae94b666fbc12308c25a32c42e4b336c
Jpecina <31221531+Jpecina@users.noreply.github.com>
2018-01-24 18:17:17 -0600

    progress

--- higher-order.js
+++ higher-order.js
@@ -20,8 +20,8 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+
+let evenNumbers  = mixedNumbers.filter(element => element % 2 === 0)
 
 
 
@@ -44,7 +44,7 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(val => val *= 1.07);
 
 
 
@@ -62,7 +62,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
   (runningTotal, curElement, curIndex, wholeArray)=>{} Arrow Form
 */
 
-//Code Here
+// let totalPopulation = populations.reduce(function(i){
+
+// })
 let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
 
 
@@ -88,9 +90,8 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
   Use the filter method to return only the monsters that have a CP of over 200.
 */
 
-//Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
 
+let myStrongest = monstersInYourPocket.filter(element => element["CP"]> 200);
 
 
 ////////// PROBLEM 5 //////////
@@ -126,6 +127,9 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.reduce(function(sum, order){
+  return sum + order.bob.price;
+},0)
+
 
 
